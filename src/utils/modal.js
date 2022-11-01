3e6d188ebf34c3e2f9e35924dd6671968e461555
sanga.park <sagna.park@epikar.com>
2022-11-01 15:59:25 +0900

    util modal.js example

---
+++ src/utils/modal.js
@@ -0,0 +1,16 @@
+// import { Modal } from "@mui/material"
+
+// const onToggleModal = () => {
+//     setModalToggle(prev => !prev)
+// }
+
+// const onClickSuccess = (msg) => {
+//     // Modal.success({ content: msg})
+// }
+
+// const onClickFailure = () => {
+//     Modal.error({
+//         title:"this is an error massage",
+//         content: "실패하였습니다."
+//     })
+// }
